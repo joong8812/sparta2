7e12c5acde157433a274b3e8866f2ac66d532bbf
jungjaechoi <joong8812@gmail.com>
2022-02-12 17:12:20 +0900

    add - django testcase

--- sparta/settings.py
+++ sparta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,9 @@ SECRET_KEY = "django-insecure-73a9s0pf#r%=%_xgwd*!mh(50mee&z8fg2bogjt+d-+6n(+wzc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = [
+    'testserver',
+]
 
 
 # Application definition
