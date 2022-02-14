b34bb5c53679a7970808c2ac5bad337e784e6e4d
jungjaechoi <joong8812@gmail.com>
2022-02-14 09:57:10 +0900

    add - local_settings 추가

--- sparta/settings.py
+++ sparta/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS: List[str] = [
 # Application definition
 
 INSTALLED_APPS = [
+    "tabom.apps.TabomConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -87,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "sparta",
         "USER": "root",
-        "PASSWORD": "qwer1234",
+        "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "3306",
     }
@@ -134,3 +135,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from sparta.local_settings import *
+except ImportError:
+    pass
