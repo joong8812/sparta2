bde205e2b30c34bcc0a0a21d6aa15c10ca1e3687
jungjaechoi <joong8812@gmail.com>
2022-02-15 13:48:08 +0900

    add - create article feature&test

--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from tabom.models import Like, User
 from tabom.models.article import Article
 from tabom.services.article_service import (
+    create_an_article,
     delete_an_article,
     get_an_article,
     get_article_list,
@@ -11,10 +12,20 @@ from tabom.services.like_service import do_like
 
 
 class TestArticleService(TestCase):
+    def test_you_can_create_an_article(self) -> None:
+        # Given
+        title = "test_title"
+
+        # When
+        article = create_an_article(title)
+
+        # Then
+        self.assertEqual(article.title, title)
+
     def test_you_can_get_an_article_by_id(self) -> None:
         # Given
         title = "test_title"
-        article = Article.objects.create(title=title)
+        article = create_an_article(title)
 
         # When
         result_article = get_an_article(0, article.id)
@@ -34,7 +45,7 @@ class TestArticleService(TestCase):
     def test_get_article_list_should_prefetch_like(self) -> None:
         # Given
         user = User.objects.create(name="test_user")
-        articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]
+        articles = [create_an_article(f"{i}") for i in range(1, 21)]
         do_like(user.id, articles[-1].id)
 
         # When
@@ -53,9 +64,9 @@ class TestArticleService(TestCase):
     def test_get_article_list_should_contain_my_likes_when_like_exists(self) -> None:
         # Given
         user = User.objects.create(name="test_user")
-        article1 = Article.objects.create(title="artice1")
+        article1 = create_an_article("artice1")
         like = do_like(user.id, article1.id)
-        Article.objects.create(title="article2")
+        create_an_article("article2")
 
         # When
         articles = get_article_list(user.id, 0, 10)
@@ -67,9 +78,9 @@ class TestArticleService(TestCase):
     def test_get_article_list_should_not_contain_my_likes_when_user_id_is_zero(self) -> None:
         # Given
         user = User.objects.create(name="test_user")
-        article1 = Article.objects.create(title="artice1")
+        article1 = create_an_article("artice1")
         Like.objects.create(user_id=user.id, article_id=article1.id)
-        Article.objects.create(title="article2")
+        create_an_article("article2")
         invalid_user_id = 0
 
         # When
@@ -82,7 +93,7 @@ class TestArticleService(TestCase):
     def test_you_can_delete_an_article(self) -> None:
         # Given
         user = User.objects.create(name="user1")
-        article = Article.objects.create(title="artice1")
+        article = create_an_article("artice1")
         like = do_like(user.id, article.id)
 
         # When
