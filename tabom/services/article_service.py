fe2393114ffc15cd1cb69ab7a5b02016189b0ef0
jungjaechoi <joong8812@gmail.com>
2022-02-15 09:20:16 +0900

    add - search articles feature & test

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,5 +1,15 @@
 from tabom.models import Article
+from django.db.models import QuerySet
+from django.core.paginator import Paginator, Page
 
 
 def get_an_article(article_id: int) -> Article:
-    return Article.objects.filter(id=article_id).get()
+    article = Article.objects.filter(id=article_id).get()
+    return article;
+        
+
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id")[offset : offset + limit]
+
+def get_article_page(page: int, limit: int) -> Page:
+    return Paginator(Article.objects.order_by("-id"), limit).page(page)
