4954aab83d18c46b1d6e4b21753b282eea16df2c
jungjaechoi <joong8812@gmail.com>
2022-02-17 01:21:37 +0900

    fix - update like on same time

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -1,13 +1,13 @@
 from tabom.models import Article, Like, User
+from django.db.models import F
 
 
 def do_like(user_id: int, article_id: int) -> Like:
     User.objects.filter(id=user_id).get()
-    article = Article.objects.filter(id=article_id).get()
+    Article.objects.filter(id=article_id).get()
 
     like = Like.objects.create(user_id=user_id, article_id=article_id)
-    article.like_count += 1
-    article.save()
+    Article.objects.filter(id=article_id).update(like_count=F("like_count") + 1)
 
     return like
 
@@ -15,6 +15,4 @@ def do_like(user_id: int, article_id: int) -> Like:
 def undo_like(user_id: int, article_id: int) -> None:
     deleted_cnt, _ = Like.objects.filter(user_id=user_id, article_id=article_id).delete()
     if deleted_cnt:
-        article = Article.objects.filter(id=article_id).get()
-        article -= 1
-        article.save()
+        Article.objects.filter(id=article_id).update(like_count=F("like_count") - 1)
